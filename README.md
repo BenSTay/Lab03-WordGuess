a7ea930724dd9dd1fbe4947b94fb36cbb85aa265
Benjamin Taylor <staylor.ben@gmail.com>
2018-06-07 11:23:24 -0700

    Added CRUD operations.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Lab03-WordGuess
+**Author**: Benjamin Taylor  
+**Version**: 1.0.0
+
+## Overview
+
+
+## Getting Started
+1. Create a fork of this repository, and clone your fork to your device.  
+2. Open the solution file (UnitTesting.sln) in Visual Studio.
+3. To run the app, click the green arrow button labeled "Start".
+4. For testing, navigate to the BankTest project using the Solution Explorer.
+5. To run the tests, press CTRL+R or navigate to Tests > Run > All Tests in the top-level menu.
+
+## Architecture
+**Languages Used**: C#  
+
+Written with Visual Studio Community 2017.
+
+## Change Log
+06-0x-2018 XX:XXxm - Initial version.
---
+++ WordGuess/WordGuess/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace WordGuess
+{
+    public class Program
+    {
+        static void CreateFile(string path, string[] content)
+        {
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    foreach (string line in content)
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+        }
+
+        static string[] ReadFile(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+
+        static void UpdateFile(string path, string line)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(line.ToUpper());
+            }
+        }
+
+        static void DeleteFile(string path)
+        {
+            File.Delete(path);
+        }
+
+        static void DisplayWords(string path)
+        {
+            string[] words = ReadFile(path);
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void CRUDTest(string path, string[] defaultWords)
+        {
+            CreateFile(path, defaultWords);
+            DisplayWords(path);
+            UpdateFile(path, Console.ReadLine());
+            DisplayWords(path);
+            Console.ReadKey();
+            DeleteFile(path);
+        }
+
+        static void Main(string[] args)
+        {
+            string path = "../../../../../words.txt";
+            string[] defaultWords = new string[] { "DECLARED", "PROPERLY", "COEFFICIENT", "ENIGMATIC", "QUESTIONING" };
+            CRUDTest(path, defaultWords);
+        }
+    }
+}
